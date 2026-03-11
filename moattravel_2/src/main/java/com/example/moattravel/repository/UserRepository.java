bc9dbe03f3c7f19b129491515fe2761d0d5af96d
sima00rnmr <sima92rnmr@gmail.com>
2026-03-11 11:23:13 +0900

    19章終了

--- moattravel_2/src/main/java/com/example/moattravel/repository/UserRepository.java
+++ moattravel_2/src/main/java/com/example/moattravel/repository/UserRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel.entity.User;
 
+public interface UserRepository extends JpaRepository<User, Integer> {
 
-public interface UserRepository extends JpaRepository<User,Integer>{
+	public User findByEmail(String email);
 
 }
