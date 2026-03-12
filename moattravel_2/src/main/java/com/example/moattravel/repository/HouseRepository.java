6f628047af856aed0503f2704a40319c637d9bdb
sima00rnmr <sima92rnmr@gmail.com>
2026-03-12 11:24:28 +0900

    27章終了

--- moattravel_2/src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ moattravel_2/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -7,5 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.moattravel.entity.House;
 
 public interface HouseRepository extends JpaRepository<House, Integer> {
+	/*ど忘れ…
+	 * インターフェースはメソッドの詳細を書かないから;で終わって良い
+	 * ので｛｝の形式を使わない
+	 * */
 	public Page<House> findByNameLike(String keyword, Pageable pageable);
+	
+	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
+	public Page<House> findByAddressLike(String area, Pageable pageable);
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
 }
